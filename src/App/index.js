1265b4df0c3f2ff62bddcaf685e613219c99733b
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-04-20 18:33:21 -0700

    created joke generator

--- src/App/index.js
+++ src/App/index.js
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from 'reactstrap';
 import './App.scss';
+import getJokes from '../helpers/data/jokeData';
 
 function App() {
-  // const [domWriting, setDomWriting] = useState('Nothing Here!');
-  const [randomJoke, setRandomJoke] = useState({});
+  const [allJokes, setAllJokes] = useState([]);
+  const [singleJoke, setSingleJoke] = useState({});
+  const [showPunchline, setShowPunchline] = useState(false);
 
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-  //   setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
+  const handleClick = () => {
+    if (showPunchline) {
+      setShowPunchline(false);
+      setSingleJoke(allJokes[Math.floor(Math.random() * allJokes.length)]);
+    } else {
+      setShowPunchline(true);
+    }
   };
 
-  return(
-    <>
-      <h1>Joke Generator</h1>
-      <button onClick={handleClick}>GET A JOKE</button>
-    </>
+  useEffect(() => {
+    getJokes()
+      .then((jokes) => {
+        setAllJokes(jokes);
+        setSingleJoke(jokes[Math.floor(Math.random() * jokes.length)]);
+      });
+  }, []);
+
+  return (
+    <div className='App'>
+      <div>
+        <h1>Joke Generator</h1>
+        <h2>{singleJoke.setup}</h2>
+        <p>{showPunchline && singleJoke.punchline}</p>
+      </div>
+      <Button color="info" onClick={handleClick}>{showPunchline ? 'GET ANOTHER JOKE' : 'GET PUNCHLINE'}</Button>
+    </div>
   );
 }
 
